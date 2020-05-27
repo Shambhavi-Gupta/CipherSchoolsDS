a1e5866d664d60628ba83619c81c1f4d164881a5
Shambhavi Gupta <gshambhavi9@gmail.com>
2020-05-27 13:26:01 +0530

    reverse of words

--- MyJavaPrograms/src/program/reverseWord.java
+++ MyJavaPrograms/src/program/reverseWord.java
@@ -1,10 +1,31 @@
 package program;
+import java.util.*;
+import java.util.Scanner;
 
 public class reverseWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc=new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+		 
+		 {
+		     String s=sc.next();
+		     String str="";
+		     int l=s.length();
+		     String[] arr=s.split("\\.");
+		     for(int i=arr.length-1;i>=0;i--)
+		     {
+		         str=str+arr[i];
+		         if(i>0)
+		         {
+		             str=str+".";
+		         }
+		       System.out.println(str);  
+		     }
+		 }t--;
 	}
 
 }
