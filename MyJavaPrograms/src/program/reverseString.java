2c75e739f634249e362f3d444ba788f986cf2ab1
Shambhavi Gupta <gshambhavi9@gmail.com>
2020-05-27 13:08:37 +0530

    reverse string

--- MyJavaPrograms/src/program/reverseString.java
+++ MyJavaPrograms/src/program/reverseString.java
@@ -1,10 +1,39 @@
 package program;
-
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 public class reverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		 Scanner sc = new Scanner(System.in);
+	     int t = sc.nextInt();
+	     while(t > 0)
+	     {
+	     String sent = sc.next();
+	     String[] strArray = sent.split("\\.");
+	     String result;
+	     for (int i = 0; i < strArray.length; i++) 
+	     {
+	     result = "";
+	     
+	     for (int j = strArray[i].length() - 1; j >= 0; j--)  
+	     {
+	     result = result + strArray[i].charAt(j);
+	     }
+	  System.out.print(result);
+	 // System.out.println();
+	   if(i != strArray.length - 1) 
+	   {
+	     System.out.print(".");
+	   }
+	  }
+	System.out.println();
+	t--;
+		
+		 }
+	}
 	}
 
-}
+
