2ecdee8fc70a1b28fdd657e2e6bd703c89c36b89
Shambhavi Gupta <gshambhavi9@gmail.com>
2020-06-07 23:58:36 +0530

    Simple recursive function

---
+++ MyJavaPrograms/src/program/recursiveFunction.java
@@ -0,0 +1,25 @@
+package program;
+
+public class recursiveFunction {
+    static void fun (int n)
+    {
+  	 
+  	  if(n>=0)
+  	  {
+  		 // return;
+  		  fun(n-1);
+      	  System.out.println(n);
+  	  }
+  	 
+  	  
+
+    }
+
+ public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fun(5);
+	}
+
+	}
+
+
